ab4598e1ed04f9f8a0e7d65677e1a81eabc2d183
kkl4846 <kkl4846@cau.ac.kr>
2022-01-10 15:24:00 +0900

    2202110_11720번 문제 풀이2

--- 문제풀이/김경린/220110_11720.py
+++ 문제풀이/김경린/220110_11720.py
@@ -1,9 +1,19 @@
 # 숫자의 합
 
 
+# case = int(input())
+
+# temp = input()
+# nums = list(map(int, temp))
+
+# print(sum(nums))
+
+# 2번째 풀이 숫자의 개수 활용
+
 case = int(input())
 
 temp = input()
-nums = list(map(int, temp))
-
-print(sum(nums))
+sum = 0
+for i in range(case):
+    sum += int(temp[i])
+print(sum)

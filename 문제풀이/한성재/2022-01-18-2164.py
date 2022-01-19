971cbd01821be1eaf4d73a3e3dbb66be1364c3f5
HanSeongJae <fhqhxm8899@gmail.com>
2022-01-20 00:25:22 +0900

    2022-01-19 문제풀이

--- 문제풀이/한성재/2022-01-18-2164.py
+++ 문제풀이/한성재/2022-01-18-2164.py
@@ -1,23 +1,20 @@
 # 2164번 카드2
-from hashlib import new
+
+import collections
 
 
-def remove_odd(arr: list) -> list:
-    new_arr = []
-    for i in range(1, len(arr)+1):
-        if i % 2 == 0:
-            new_arr.append(arr[i-1])
-        else:
-            continue
+def new_deck(arr: list) -> None:
 
-    return new_arr
+    deq = collections.deque(arr)
+    while(len(deq) > 1):
+        deq.popleft()
+        deq.rotate(-1)
+
+    print(deq.pop())
 
 
 num = int(input())
 
 arr = list(range(1, num+1))
 
-while len(arr) > 1:
-    arr = remove_odd(arr)
-
-print(arr[0])
+new_deck(arr)

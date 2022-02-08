7c2df13e1c4e3134de6fadb0122cae3b59d8821f
kkl4846 <kkl4846@cau.ac.kr>
2022-02-09 00:44:30 +0900

    1406번 시간초과는 해결 but 틀림

--- 문제풀이/김경린/220208_1406.py
+++ 문제풀이/김경린/220208_1406.py
@@ -1,27 +1,33 @@
 
 
-strs = list(input())
-cursor = len(strs)
+from sys import stdin
 
-num = int(input())
+strs = list(stdin.readline())
+tmp = []
+
+
+num = int(stdin.readline())
 
 for i in range(num):
-    command = list(map(str, input().split()))
+    command = list(map(str, stdin.readline().split()))
     if len(command) > 1:
         if command[0] == 'P':
-            strs.insert(cursor, command[1])
-            cursor += 1
+            strs.append(command[1])
     else:
         if command[0] == 'L':
-            if cursor > 0:
-                cursor -= 1
+            if len(strs) > 0:
+                char = strs.pop()
+                tmp.append(char)
         elif command[0] == 'D':
-            if cursor < len(strs):
-                cursor += 1
+            if tmp:
+                char = tmp.pop()
+                strs.append(char)
         elif command[0] == 'B':
-            if cursor > 0:
-                cursor -= 1
-                strs.pop(cursor)
+            if strs:
+                strs.pop()
+
 
 for i in strs:
     print(i, end='')
+for i in range(len(tmp)):
+    print(tmp.pop(), end='')

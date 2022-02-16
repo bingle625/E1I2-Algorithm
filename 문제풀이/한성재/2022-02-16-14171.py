fd45bf4230484c1396867ac629bf7d06319dd9ae
HanSeongJae <fhqhxm8899@gmail.com>
2022-02-16 21:26:10 +0900

    14171 예외를 찾지 못하고 있음

--- 문제풀이/한성재/2022-02-16-14171.py
+++ 문제풀이/한성재/2022-02-16-14171.py
@@ -1,31 +1,35 @@
 # 14171번
 # Cities and States
 
+import collections
+from filecmp import dircmp
 from sys import stdin
 
 
 num = int(stdin.readline())
 
 city = {}
-state_code = []
-city_parsed = []
+used = {}
+city_states = []
 pairs = 0
 
 for _ in range(num):
     inp = stdin.readline().split()
-    # city.append(inp[0])
-    city_parsed.append(inp[0][0:2])
-    state_code.append(inp[1])
+    city_state = inp[0][0:2] + inp[1]
+    city_states.append(city_state)
 
-    city[inp[0]]
+dict = collections.Counter(city_states)
 
-    for idx in range(len(city_parsed)-1):
-        if inp[0][0:2] == state_code[idx]:
-            if city_parsed[idx] == inp[1]:
-                pairs += 1
-            else:
-                continue
-        else:
+for key1 in dict.keys():
+    key2 = key1[2:4] + key1[0:2]
+    if key2 in dict:
+        if key2 in used:
             continue
+        else:
+            pairs += dict.get(key1) * dict.get(key2)
+            used[key1] = True
+            used[key2] = True
 
+print(dict)
+print(used)
 print(pairs)

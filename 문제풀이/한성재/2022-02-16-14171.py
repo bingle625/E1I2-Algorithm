6778e850b17fc98e6abae3d0fdde95ef39c9701c
HanSeongJae <fhqhxm8899@gmail.com>
2022-02-16 20:50:15 +0900

    반복문을 이용한 풀이는 시간초과

--- 문제풀이/한성재/2022-02-16-14171.py
+++ 문제풀이/한성재/2022-02-16-14171.py
@@ -6,27 +6,26 @@ from sys import stdin
 
 num = int(stdin.readline())
 
-city = []
+city = {}
 state_code = []
 city_parsed = []
 pairs = 0
 
 for _ in range(num):
     inp = stdin.readline().split()
-    city.append(inp[0])
+    # city.append(inp[0])
     city_parsed.append(inp[0][0:2])
     state_code.append(inp[1])
 
-for idx in range(len(city)):
-    for idx2 in range(len(state_code)):
-        if city_parsed[idx] == state_code[idx2]:
-            if city_parsed[idx2] == state_code[idx]:
+    city[inp[0]]
 
-                # 배열로 할 경우 인덱스 중복 문제에서 벗어나기가 어려움.
-
-                # for val in city_state_dic.values():
-                #     for key in city_state_dic.keys():
-                #         if val == key[0:2]:
-                #             if city_state_dic.get(key)
+    for idx in range(len(city_parsed)-1):
+        if inp[0][0:2] == state_code[idx]:
+            if city_parsed[idx] == inp[1]:
+                pairs += 1
+            else:
+                continue
+        else:
+            continue
 
 print(pairs)

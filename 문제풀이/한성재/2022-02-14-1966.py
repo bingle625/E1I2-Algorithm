1a0235a57080cf54c34c1aa39cffa266bffdd470
HanSeongJae <fhqhxm8899@gmail.com>
2022-02-16 15:55:31 +0900

    1966번 풀었으나 누더기 코드

--- 문제풀이/한성재/2022-02-14-1966.py
+++ 문제풀이/한성재/2022-02-14-1966.py
@@ -4,10 +4,13 @@ from sys import stdin
 k = int(stdin.readline())
 
 queue_num = collections.deque([])
-queue_flag = collections.deque([])
 
 for _ in range(k):
-    prioriy = 0
+    queue_flag = collections.deque([])
+
+    priority = 1
+    max = 0
+
     num = list(map(int, stdin.readline().split()))
 
     for i in range(num[0]):
@@ -16,18 +19,24 @@ for _ in range(k):
         else:
             queue_flag.append(False)
     queue_num = collections.deque(list(map(int, stdin.readline().split())))
-    print(queue_flag)
 
-    # 여기서 iteration 마다 queue의 순서가 바뀌어서, 해당 elem번째의 요소가 원한 데로 뽑히지 않아서 일어나는 오류
-    # sort함수를 사용하면 flag큐를 sortiong 할 방법이 없으므로, 내가 sort해서, flag도 같이 sort해야함.
-    for elem in range(len(queue_num)):
-        if queue_num[elem] > queue_num[0]:
+    while queue_num:
+        for k in range(len(queue_num)):
+            if max < queue_num[k]:
+                max = queue_num[k]
+
+        while queue_num[0] != max:
             queue_num.append(queue_num.popleft())
             queue_flag.append(queue_flag.popleft())
-            continue
-    print(queue_flag)
 
-    for elem in range(len(queue_flag)):
-        prioriy += 1
-        if queue_flag.popleft() == True:
-            print(prioriy)
+        if queue_flag[0] == True:
+            break
+        queue_num.popleft()
+        queue_flag.popleft()
+        priority += 1
+        max = 0
+
+    print(priority)
+
+# 여기서 iteration 마다 queue의 순서가 바뀌어서, 해당 elem번째의 요소가 원한 데로 뽑히지 않아서 일어나는 오류
+# sort함수를 사용하면 flag큐를 sortiong 할 방법이 없으므로, 내가 sort해서, flag도 같이 sort해야함.
--- 문제풀이/한성재/test.py
+++ 문제풀이/한성재/test.py
@@ -1,39 +1,46 @@
-
-def dfs(graph, start_node, cnt):
-    stack = list()
-    visit = list()
-    stack.append(start_node)
-
-    while stack:
-        node = stack.pop()
-
-        if node >= 0 and node < M:
-            cnt += 1
-            visit = list()
-        if node not in visit:
-            visit.append(node)
-            stack.extend(graph[node])
-    return cnt
+import heapq
+from typing import List
 
 
-N, M = input().split()
-N = int(N)
-M = int(M)
-board = [[int(x) for x in input().split()]for y in range(N)]
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-glass = {}
-for height in range(N):
-    for width in range(M):
-        if board[height][width] == 1:
-            strong = []
-            for i in dx:
-                if width+dx[i] >= 0 and width+dx[i] < M and height-1 < N and height-1 >= 0:
-                    if board[height-1][width+dx[i]] == 1:
-                        strong.append(
-                            (height-1)M+width+dx[i])
-            glass[Mheight+width] = strong
-cnt = 0
-for key in glass:
-    if key >= (N-1)M and key < NM:
-        cnt = dfs(glass, key, cnt)
-print(cnt % 1000000007)
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        root = result = ListNode(None)
+        heap = []
+
+        # 각 연결 리스트의 루트를 힙에 저장
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i, lists[i]))
+
+        # 힙 추출 이후 다음 노드는 다시 저장
+        while heap:
+            node = heapq.heappop(heap)
+            idx = node[1]
+            result.next = node[2]
+
+            result = result.next
+            if result.next:
+                heapq.heappush(heap, (result.next.val, idx, result.next))
+
+        return root.next
+
+
+s = Solution()
+list = ListNode([1, 2, 3])
+lists = [[1, 4, 5], [1, 3, 4], [2, 6]]
+print(list)
+print(s.mergeKLists())

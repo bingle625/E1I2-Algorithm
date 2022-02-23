231012a4548aa8e5a81a510c70b9a9733e7a41ec
kkl4846 <kkl4846@cau.ac.kr>
2022-02-24 02:38:47 +0900

    2178번

--- 문제풀이/김경린/220223_2178.py
+++ 문제풀이/김경린/220223_2178.py
@@ -18,27 +18,28 @@ visited.append([0,1,1])
 
 def bfs():
 
-    min = M*N
-    while visited:
+    while True:
         tmp = visited.popleft()
         
         current_cnt ,current_x,current_y = tmp[0],tmp[1],tmp[2]
     
-        
-        current_cnt+=1
-        maze[current_y-1][current_x-1]=0
-        
-        for i in range(4):
-            next_x = current_x + dx[i]
-            next_y = current_y + dy[i]
-            if next_x>0 and next_y>0 and next_x<M+1 and next_y<N+1  and maze[next_y-1][next_x-1]=='1':
-                visited.append([current_cnt,next_x,next_y])
+        if maze[current_y-1][current_x-1]=='1':
+            current_cnt+=1
+            maze[current_y-1][current_x-1]=0
+            
+            if current_x == M and current_y==N:
+                return current_cnt
+
+            for i in range(4):
+                next_x = current_x + dx[i]
+                next_y = current_y + dy[i]
+                if next_x>0 and next_y>0 and next_x<M+1 and next_y<N+1  and maze[next_y-1][next_x-1]=='1':
+                    visited.append([current_cnt,next_x,next_y])
 
 
-                if next_x==M and next_y==N:
-                    return current_cnt+1
+                    
 
-                
+                    
 
                 
                 

97c22c8ddb45b1b5bc421f9550bf7a3739885b3f
kkl4846 <kkl4846@cau.ac.kr>
2022-04-04 23:53:14 +0900

    1202

--- 문제풀이/김경린/220401_1202.py
+++ 문제풀이/김경린/220401_1202.py
@@ -1,6 +1,6 @@
 
 #가방은 무게가 작은거 부터
-#보석을 비싼 거 부터
+#보석은 가벼운거부터 넣고 그 중 가치가 높은 것을 선택
 #dict = {가격:무게}
 
 from collections import defaultdict, deque
@@ -18,7 +18,7 @@ bags = []
 
 for i in range(jewel_num):
     weight, price = map(int,stdin.readline().split())
-    heapq.heappush(jewels,(-price,weight))
+    heapq.heappush(jewels,(weight,price))
 
 for i in range(bag_num):
     bag = int(stdin.readline().strip())
@@ -28,19 +28,20 @@ bags = sorted(bags)
 
 sum =0
 
-def get_jewel(jewels,bag):
-    while len(jewels):
-        jewel = heapq.heappop(jewels)
-        if jewel[1]<=bag:
-            return jewel[0]
-        else:
-            val = get_jewel(jewels,bag)
-            heapq.heappush(jewels,(jewel[0],jewel[1]))
-            return val
-    # 담을 수 있는 보석이 없을 때
-    return 0
+availJewel = []
 
 for bag in bags:
-    sum -= get_jewel(jewels,bag)
+    
+    while len(jewels) and bag>=jewels[0][0]:
+        jewel = heapq.heappop(jewels)
+        heapq.heappush(availJewel,-jewel[1])
+    
+    if len(availJewel):
+        sum -= heapq.heappop(availJewel)
+    if not len(jewels):
+        break
+
+
+    
 
 print(sum)

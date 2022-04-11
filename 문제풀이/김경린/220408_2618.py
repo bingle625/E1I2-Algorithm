97d217cd226b9ea60ac3bf786c540aad6af11002
kkl4846 <kkl4846@cau.ac.kr>
2022-04-11 12:59:13 +0900

    2618번

--- 문제풀이/김경린/220408_2618.py
+++ 문제풀이/김경린/220408_2618.py
@@ -19,9 +19,11 @@ for i in range(case_num):
     case.append((x,y))
 
 police = [(1,1),(num,num)]
-case_police = []
+min_dp = sys.maxsize
+
 
 dp = [ [ 0 for _ in range(case_num+1)] for _ in range(case_num+1)]
+path = [ [ [] for _ in range(case_num+1)] for _ in range(case_num+1)]
 
 for a in range(case_num+1):
     for b in range(case_num+1):
@@ -29,19 +31,69 @@ for a in range(case_num+1):
             continue
         elif a>b:
             if a-b==1 and a>1:
-                dp[a][b] =  min( [dp[i][b]+dist(case[i] if i>0 else police[0],case[a-1]) for i in range(0,a-1)] ) 
+                min_dp_a = sys.maxsize
+                for i in range(0,a-1):
+                    now = dp[i][b]+dist(case[i-1] if i>0 else police[0],case[a-1])
+                    if now < min_dp_a:
+                        dp[a][b] = now
+                        min_dp_a = now
+                        path[a][b] = [i,b]
+                    elif now==min_dp_a:
+                        if dist(case[i-1] if i>0 else police[0],case[a-1])<dist(case[path[a][b][0]-1] if path[a][b][0]>0 else police[0],case[a-1]):
+                            dp[a][b] = now
+                            min_dp_a = now
+                            path[a][b] = [i,b]
             else:
                 dp[a][b] = dp[a-1][b] + dist(case[a-2] if a>1 else police[0],case[a-1])
+                path[a][b] = [a-1,b]
+
+            if a==case_num:
+                if dp[a][b]<min_dp:
+                    min_dp = dp[a][b]
+                    min_a = a
+                    min_b = b
         elif a<b:
             if b-a==1 and b>1:
-                dp[a][b] = min( [dp[a][i]+dist(case[i] if i>0 else police[1],case[b-1]) for i in range(0,b-1)] )  
+                min_dp_b = sys.maxsize
+                for i in range(0,b-1):
+                    now = dp[a][i]+dist(case[i-1] if i>0 else police[1],case[b-1])
+                    if now < min_dp_b:
+                        dp[a][b] = now
+                        min_dp_b = now
+                        path[a][b] = [a,i]
+                    elif now==min_dp_b:
+                        if dist(case[i-1] if i>0 else police[1],case[b-1])<dist(case[path[a][b][1]-1] if path[a][b][1]>0 else police[1],case[b-1]):
+                            dp[a][b] = now
+                            min_dp_b = now
+                            path[a][b] = [a,i]
             else:
                 dp[a][b] = dp[a][b-1] + dist(case[b-2] if b>1 else police[1],case[b-1])
+                path[a][b] = [a,b-1]
+            if b==case_num:
+                if dp[a][b]<min_dp:
+                    min_dp = dp[a][b]
+                    min_a = a
+                    min_b = b
 
-end = []
-for x in range(case_num+1):
-    for y in range(case_num+1):
-        if not (x==case_num and y==case_num) and (x==case_num or y==case_num) :
-            end.append(dp[x][y])
-print(min(end))
-    
+print(dp[min_a][min_b])
+
+go = []
+
+
+while not (min_a==0 and min_b==0):
+    if min_a>min_b:
+        go.append(1)
+    else:
+        go.append(2)
+    min_a,min_b = path[min_a][min_b]
+
+
+
+for i in range(len(go)-1,-1,-1):
+    print(go[i])
+            
+
+
+
+
+

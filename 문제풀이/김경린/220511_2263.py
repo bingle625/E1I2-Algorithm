3d850cb5b77dc174cc153ef245252f7c4d5051d5
kkl4846 <kkl4846@cau.ac.kr>
2022-05-12 03:38:51 +0900

    [Chore] 2263번 index풀이

--- 문제풀이/김경린/220511_2263.py
+++ 문제풀이/김경린/220511_2263.py
@@ -1,35 +1,43 @@
 from collections import defaultdict, deque
 from sys import stdin
+import sys
+sys.setrecursionlimit(10**9)
+
 
 nodeNum = int(stdin.readline())
 inOrder = list(map(int, stdin.readline().split()))
 postOrder = list(map(int, stdin.readline().split()))
 
+pos = [0]*(nodeNum+1)
+for i in range(nodeNum): 
+    pos[inOrder[i]] = i
 
 
-def findTree(inOrder,postOrder):
 
-    parent = postOrder[-1]
-    parentIdx = inOrder.index(parent)
-    leftIn = inOrder[0:parentIdx]
-    leftPost = postOrder[0:parentIdx]
+def findTree(inOrderStart,inOrderEnd,postOrderStart,postOrderEnd):
+
+    parent = postOrder[postOrderEnd]
+    parentIdx = pos[parent] - inOrderStart #상대적으로 계산
     print(parent, end = ' ')
 
-    if leftIn:
-        # if len(leftIn) == 1:
-        #     tree[parent].append(leftIn[0])
-        # else:
-            findTree(leftIn, leftPost)
+    # leftIn = inOrder[0:parentIdx]
+    # leftPost = postOrder[0:parentIdx]
+
+    if inOrderStart <= inOrderStart+parentIdx-1:
+        if inOrderStart == inOrderStart+parentIdx-1:
+            print(inOrder[inOrderStart], end = ' ')
+        else:
+            findTree(inOrderStart, inOrderStart+parentIdx-1, postOrderStart, postOrderStart+parentIdx-1)
 
 
-    rightIn = inOrder[parentIdx+1:]
-    rightPost = postOrder[parentIdx:-1]
+    # rightIn = inOrder[parentIdx+1:]
+    # rightPost = postOrder[parentIdx:-1]
+    if  inOrderStart+parentIdx + 1 <= inOrderEnd:
+        if inOrderStart+parentIdx + 1 == inOrderEnd:
+            print(inOrder[inOrderEnd], end = ' ')
 
-    if rightIn:
-        # if len(rightIn) == 1:
-        #     tree[parent].append(rightIn[0])
-        
-        # else:
-            findTree(rightIn,rightPost)
+        else:
+            findTree(inOrderStart+parentIdx + 1, inOrderEnd, postOrderStart+parentIdx, postOrderEnd - 1)
     
-findTree(inOrder, postOrder)
+findTree(0, len(inOrder)-1, 0,len(postOrder)-1)
+

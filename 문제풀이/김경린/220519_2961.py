4a9aacd292bdb90ddcaa22123d78cff081f4c3c9
kkl4846 <kkl4846@cau.ac.kr>
2022-05-19 18:44:52 +0900

    [ADD] 2961 비트마스킹

--- 문제풀이/김경린/220519_2961.py
+++ 문제풀이/김경린/220519_2961.py
@@ -21,8 +21,24 @@ for i in range(num):
     sour.append(s)
     bitter.append(b)
 
-for i in range(1,num+1):
-    for j in list(combinations([k for k in range(0,num)],i)):
-        minValue = min(minValue,findMin(j))
+# 브루트 포스
+# for i in range(1,num+1):
+#     for j in list(combinations([k for k in range(0,num)],i)):
+#         minValue = min(minValue,findMin(j))
+
+# 비트 마스크 (combination을 비트마스크로)
+# ex) num = 4이면
+# 1 ~  10000까지 각각 포함되는 경우, 안되는 경우를 0,1로 표현
+# 0111은 idx 0,1,2는 포함 3은 미포함
+for i in range(1, 1 << num):
+    s = 1
+    b = 0
+    for j in range(num):
+        if i & 1 << j:
+            s *= sour[j]
+            b += bitter[j]
+    minValue = min(minValue,abs(s-b))
+
+
 
 print(minValue)

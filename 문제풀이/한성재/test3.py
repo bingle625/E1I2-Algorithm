6e83f6bb436276c6af41705501ebd85694fd84ca
HanSeongJae <fhqhxm8899@gmail.com>
2022-07-02 22:12:43 +0900

    1149 RGB거리

--- 문제풀이/한성재/test3.py
+++ 문제풀이/한성재/test3.py
@@ -1,19 +1,43 @@
-import matplotlib.pyplot as plt
-data = {'Frogs': 30, 'Hogs': 40, 'Dogs': 10, 'Logs': 20}
-print("data = ", data)
+def solution(new_id: str):
+    temp = new_id
+    temp = temp.lower()
+    arr = []
+    for char in temp:
+        if char not in ("abcdefghijklmnopqrstuvwxyz1234567890-_."):
+            arr.append(char)
+            arr = set(arr)
+            arr = list(arr)
+    print(arr)
+    for char in arr:
+        temp = temp.replace(char, "")
+        print(temp)
 
-data = sorted(data.items(), key=lambda x: x[1])
-print("sorted data = ", data)
-labels = list()
-sizes = list()
-for item in data:
-    labels.append(item[0])
-    sizes.append(item[1])
-print("labels=", labels)
-print("sizes = ", sizes)
-explode = (0, 0, 0, 0.1)
-ax = plt.axes()
-ax.pie(sizes, explode=explode, labels=labels,
-       autopct='%.0f%%', shadow=True, startangle=90)
-ax.axis('equal')
-plt.show()
+    temp2 = ""
+    while temp2 != temp:
+        temp2 = temp
+        temp = temp.replace("..", ".")
+
+    if temp[0] == ".":
+        temp = temp[1:]
+    if temp[-1] == ".":
+        temp = temp[:-1]
+
+    if len(temp) == 0:
+        temp = "a"
+    elif len(temp) >= 16:
+        temp = temp[:15]
+        if temp[-1] == ".":
+            temp = temp[:-1]
+
+    if len(temp) <= 2:
+        temp = list(temp)
+        while len(temp) < 3:
+            temp.append(temp[-1])
+
+    answer = ''.join(temp)
+    return answer
+
+
+s = solution("!@#asldknsadasdasdlknsa@!#@#!12312425")
+
+print(s)

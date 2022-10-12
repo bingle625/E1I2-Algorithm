498d789abf26b98ca1a8a155321a0b8a19afefc6
kkl4846 <kkl4846@cau.ac.kr>
2022-10-13 01:18:03 +0900

    17298번

---
+++ 문제풀이/김경린/221012_17298.py
@@ -0,0 +1,15 @@
+
+from sys import stdin
+nums_len = int(stdin.readline().strip())
+nums = list(map(int, stdin.readline().split()))
+
+nge = [ -1 for _ in range(nums_len)]
+tmp = []
+for i in range(len(nums)):
+    while len(tmp) and nums[i]>tmp[-1][0]:
+        idx = tmp.pop()[1]
+        nge[idx] = nums[i]
+    tmp.append((nums[i],i))
+
+for i in range(len(nge)):
+    print(nge[i], end=' ')

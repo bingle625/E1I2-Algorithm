d46a01656ec5ac95a034c9a3832b291f48a97102
kkl4846 <kkl4846@cau.ac.kr>
2022-10-15 23:01:39 +0900

    10845번

---
+++ 문제풀이/김경린/221015_10845.py
@@ -0,0 +1,36 @@
+from collections import deque
+from sys import stdin
+
+instructionsNum = int(stdin.readline().strip())
+
+deq = deque()
+while instructionsNum:
+    instruction = list(stdin.readline().strip().split())
+    if len(instruction)>1:
+        if instruction[0] == 'push':
+            deq.append(instruction[1])
+    else:
+        if instruction[0] == 'front':
+            if len(deq):
+                print(deq[0])
+            else:
+                print(-1)
+        elif instruction[0] == 'pop':
+            if len(deq):
+                print(deq.popleft())
+            else:
+                print(-1)
+        elif instruction[0] == 'size':
+            print(len(deq))
+        elif instruction[0] == 'empty':
+            if len(deq):
+                print(0)
+            else:
+                print(1)
+        elif instruction[0] == 'back':
+            if len(deq):
+                print(deq[-1])
+            else:
+                print(-1)
+    instructionsNum -= 1
+

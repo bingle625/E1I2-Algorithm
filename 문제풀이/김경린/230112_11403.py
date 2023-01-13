584972ce51471114325cb9cccba855c1ac620292
kkl4846 <kkl4846@cau.ac.kr>
2023-01-13 18:10:58 +0900

    11403번(bfs,dfs, 플로이드)

--- 문제풀이/김경린/230112_11403.py
+++ 문제풀이/김경린/230112_11403.py
@@ -1,4 +1,8 @@
-from collections import defaultdict
+# bfs, dfs, 플로이드-워셜 3가지 방식으로 풀어보기
+
+
+
+from collections import defaultdict, deque
 from operator import truediv
 
 
@@ -10,13 +14,53 @@ for i in range(n):
     v.append(v_)
 
 
-for k in range(n):
-    for i in range(n):
-        for j in range(n):
-            if v[i][k] and v[k][j]:
-                v[i][j] = 1
 
-for i in range(n):
+def dfs(i, visited):
     for j in range(n):
-        print(v[i][j], end=' ')
+        if not visited[j] and v[i][j] == 1:
+            visited[j] = 1
+            dfs(j, visited)
+    
+for i in range(n):
+    visited = [ 0 for _ in range(n)]
+    dfs(i, visited)
+    for j in range(n):
+        print(visited[j], end=' ')
     print('')
+
+
+# def bfs():
+#     for i in range(n):
+#         visited = [ 0 for _ in range(n)]
+#         visited_deque = deque()
+#         for j in range(n):
+#             if v[i][j] == 1 and not visited[j]:
+#                 visited[j] = 1
+#                 visited_deque.append(j)
+        
+#         while len(visited_deque):
+#             node = visited_deque.popleft()
+#             for j in range(n):
+#                 if v[node][j] == 1 and not visited[j]:
+#                     visited[j] = 1
+#                     v[i][j] = 1
+#                     visited_deque.append(j)
+#         for j in range(n):
+#             print(visited[j], end=' ')
+#         print('')
+    
+
+# bfs()
+
+
+# def pulid():
+    # for k in range(n):
+    #     for i in range(n):
+    #         for j in range(n):
+    #             if v[i][k] and v[k][j]:
+    #                 v[i][j] = 1
+
+    # for i in range(n):
+    #     for j in range(n):
+    #         print(v[i][j], end=' ')
+    #     print('')

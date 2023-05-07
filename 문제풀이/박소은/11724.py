4dee46588c3dc92b0db6a347135e07d1d43500cc
박소은 <71963320+separk314@users.noreply.github.com>
2023-05-07 20:22:52 +0900

    11724번

--- 문제풀이/박소은/11724.py
+++ 문제풀이/박소은/11724.py
@@ -1,45 +1,44 @@
-import collections
+# 첫째 줄에 정점의 개수 N과 간선의 개수 M이 주어진다. (1 ≤ N ≤ 1,000, 0 ≤ M ≤ N×(N-1)/2) 
+# 둘째 줄부터 M개의 줄에 간선의 양 끝점 u와 v가 주어진다. (1 ≤ u, v ≤ N, u ≠ v) 
+# 같은 간선은 한 번만 주어진다.
+
+from collections import defaultdict
 import sys
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(10000)
 
-def g_search(graph) -> int:
-    link = []   # 연결 요소
-    visited = set()     # 방문한 노드
+visited = []    # 방문한 vertex 저장
 
-    def dfs(vertex, l_set):
-        if vertex not in visited:
-            visited.add(vertex)
-            l_set.add(vertex)
-            if len(l_set) == 1:
-                link.append(l_set)
-            
-        
-        for w in graph[vertex]:
-            if w not in visited:
-                l_set = dfs(w, l_set)
-            elif w in visited and w not in l_set:
-                for elem in link:
-                    if w in elem:
-                        elem |= l_set # 집합 결합
-                        temp = l_set
-                        if l_set in link:
-                            link.remove(l_set)
-                        return temp
-        return l_set
-            
-    
-    for x in list(graph):
-        dfs(x, set())
+def DFS(root):
+    result = []
+    stack = [root]
 
-    return len(link)
-            
+    while stack:
+        v = stack.pop()
+        if v not in visited:
+            visited.append(v)
+            result.append(v)
+            for w in links[v]:
+                stack.append(w)
+    # return result
 
-# 입력
-n, m = map(int, input().split())
-graph = collections.defaultdict(list)
-for i in range(m):
-    x, y = map(int, input().split())
-    graph[x].append(y)
+N, M = map(int, input().split())    # N: vertex#, M: link#
+links = defaultdict(list)
+for _ in range(M):
+    a, b = map(int, input().split())
+    links[a].append(b)
+    links[b].append(a)
+answer = 0
 
-print(graph)
-print(g_search(graph))
+for elem in links.keys():
+    if elem not in visited:
+        DFS(elem)
+        answer += 1
+
+for i in range(1, N+1):
+    if not links[i]:    # 연결되어 있지 않은 독립된 vertex인 경우
+        answer += 1
+    elif i not in visited:  # 방문하지 않은 vertex인 경우
+        DFS(elem)
+        answer += 1
+
+print(answer)

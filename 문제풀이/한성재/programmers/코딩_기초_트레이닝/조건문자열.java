19674bdd605f5747ddbf4e8af22c7410284e8fe8
bingle625 <fhqhxm8899@gmail.com>
2023-08-29 22:10:42 +0900

    프로그래머스_조건문자열

---
+++ 문제풀이/한성재/programmers/코딩_기초_트레이닝/조건문자열.java
@@ -0,0 +1,23 @@
+package programmers.코딩_기초_트레이닝;
+
+import java.util.Objects;
+
+public class 조건문자열 {
+    class Solution {
+        public static int solution(String ineq, String eq, int n, int m) {
+            if (n == m && eq.equals("=")) {
+                return 1;
+            }
+            if (ineq.equals(">") && (n > m) || ineq.equals("<") && (n < m)) {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        int answer = Solution.solution(">","!",41,78);
+
+        System.out.println(answer);
+    }
+}

6408dee1344aa460974a008f0e4e3261b8a24681
kkl4846 <kkl4846@cau.ac.kr>
2023-11-09 02:13:51 +0900

    프로그래머스 62757 자바 시간초과 해결

--- 문제풀이/김경린/231108_62757.java
+++ 문제풀이/김경린/231108_62757.java
@@ -1,15 +1,17 @@
 import java.util.*;
 
+// dfs로 permutation 구현
+// 정답이 int를 벗어나기 때문에 long으로 구현해야한다.
 class Solution {
-    public int maxVal = 0;
-    public int calc(int[] visited, ArrayList<Integer> nums, ArrayList<String> operator, ArrayList<String> operatorSet) {
+    public long maxVal = 0;
+    public long calc(int[] visited, ArrayList<Long> nums, ArrayList<String> operator, ArrayList<String> operatorSet) {
         for(int i=0; i<visited.length; i++) {
-            int idx = visited[i];
+            int idx = visited[i]-1;
             String op = operatorSet.get(idx);
  
             for(int j=0; j<operator.size(); j++) {
                 if (operator.get(j).equals(op)) {
-                    int num = 0;
+                    long num = 0;
                     if(op.equals("+")) {
                         num = nums.get(j) + nums.get(j+1);
                     } else if(op.equals("-")) {
@@ -17,7 +19,6 @@ class Solution {
                     } else if (op.equals("*")) {
                         num = nums.get(j) * nums.get(j+1);
                     }
-                    
                 
                     nums.remove(j);
                     nums.remove(j);
@@ -32,12 +33,15 @@ class Solution {
             
             
         }
+        // System.out.println(Arrays.toString(visited) + nums.get(0));
         return Math.abs(nums.get(0));
     }
-    public void dfs(int[] visited, ArrayList<Integer> nums,  ArrayList<String> operator, ArrayList<String> operatorSet, int cnt) {
+    public void dfs(int[] visited, ArrayList<Long> nums,  ArrayList<String> operator, ArrayList<String> operatorSet, int cnt) {
         
-        if (cnt == visited.length) {
-            maxVal = Math.max(maxVal,calc(visited, (ArrayList<Integer>) nums.clone(), (ArrayList<String>) operator.clone(), operatorSet));
+        if (cnt == visited.length+1) {
+            maxVal = Math.max(maxVal,calc(visited, (ArrayList<Long>) nums.clone(), (ArrayList<String>) operator.clone(), operatorSet));
+            return ;
+        
         }
     
         for (int i = 0; i < visited.length; i++) {
@@ -53,7 +57,7 @@ class Solution {
         Queue<String> queue = new LinkedList<>();
         ArrayList<String> operatorSet = new ArrayList<String>();
         ArrayList<String> operator = new ArrayList<String>();
-        ArrayList<Integer> nums = new ArrayList<Integer>();
+        ArrayList<Long> nums = new ArrayList<Long>();
         
         String[] expressionChars = expression.split("");
         ArrayList<String> expressionSplited = new ArrayList<String>();
@@ -64,9 +68,9 @@ class Solution {
             if (expressionChars[i].equals("+") || expressionChars[i].equals("-") || expressionChars[i].equals("*")) {
                 
                 String[] num = Arrays.copyOfRange(expressionChars, lastIdx, i);
-                nums.add(Integer.parseInt(String.join("", num)));
+                nums.add(Long.parseLong(String.join("", num)));
                 
-                if (!operatorSet.contains(expressionChars)) {
+                if (!operatorSet.contains(expressionChars[i])) {
                     operatorSet.add(expressionChars[i]);
                 }
                 operator.add(expressionChars[i]);
@@ -76,12 +80,13 @@ class Solution {
         }
         
         String[] num = Arrays.copyOfRange(expressionChars, lastIdx, expressionChars.length);
-        nums.add(Integer.parseInt(String.join("", num)));
+        nums.add(Long.parseLong(String.join("", num)));
         
         // 우선순위
+        // System.out.println(operatorSet);
         int[] visited = new int[operatorSet.size()];
         
-        dfs(visited, nums, operator, operatorSet, 0);
+        dfs(visited, nums, operator, operatorSet, 1);
         return maxVal;
     }
 }
